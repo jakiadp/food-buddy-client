2983670ac2072b860d8645b95b7dfe5bb2bdda29
Jakia Akter <jakiadp@gmail.com>
2025-10-16 04:08:20 +0600

    add to banar

--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const LogIn = () => {
     return (
-        <div>
-            <p>log</p>
-        </div>
+                <div className="flex justify-center items-center min-h-screen bg-gray-100 px-4">
+      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10  bg-gray-50  text-gray-800">
+	<div className="mb-8 text-center">
+		<h1 className="my-3 text-4xl font-bold">Log in</h1>
+		<p className="text-sm  text-gray-600">Sign in to access your account</p>
+	</div>
+	<form noValidate="" action="" className="space-y-12">
+		<div className="space-y-4">
+			<div>
+				<label htmlFor="email" className="block mb-2 text-sm text-start">Email address</label>
+				<input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md  border-gray-300  bg-gray-50  text-gray-800" />
+			</div>
+			<div>
+				<div className="flex justify-between mb-2">
+					<label htmlFor="password" className="text-sm">Password</label>
+					<a rel="noopener noreferrer" href="#" className="text-xs hover:underline  text-gray-600">Forgot password?</a>
+				</div>
+				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md  border-gray-300  bg-gray-50  text-gray-800" />
+			</div>
+		</div>
+		<div className="space-y-2">
+			<div>
+				<button type="button" className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition duration-200">Sign in</button>
+			</div>
+			<p className="px-6 text-sm text-center  text-gray-600">Don't have an account yet?
+				<a rel="noopener noreferrer" href="#" className="hover:underline  text-blue-500"><Link to='/register' className='className="text-blue-500 hover:underline' >Register</Link></a>.
+			</p>
+		</div>
+	</form>
+</div>
+    </div>
     );
 };
 
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,10 +1,83 @@
-import React from 'react';
+import { Link } from "react-router";
+
 
 const Register = () => {
+  
+
+
     return (
-        <div>
-            <h1>resi</h1>
-        </div>
+         <div className="flex justify-center items-center min-h-screen bg-gray-100 px-4">
+      <div className="w-full max-w-md bg-white p-8 rounded shadow-md">
+        <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
+
+        <form  className="space-y-4">
+          <div>
+            <label className="block text-start text-sm font-medium text-gray-700">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="mt-1 w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Your name"
+             
+              required
+            />
+          </div>
+
+          <div>
+            <label className="text-start block text-sm font-medium text-gray-700">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="mt-1 w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="you@example.com"
+              
+              required
+            />
+          </div>
+
+          <div>
+            <label className=" text-start block text-sm font-medium text-gray-700">Photo URL</label>
+            <input
+              type="text"
+              name="photoURL"
+              className="mt-1 w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="https://your-photo.com/img.jpg"
+             
+              required
+            />
+          </div>
+
+          <div>
+            <label className="text-start block text-sm font-medium text-gray-700">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="mt-1 w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              placeholder="Password"
+              
+              
+              required
+            />
+          </div>
+
+          
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition duration-200"
+          >
+            Register
+          </button>
+        </form>
+
+        <p className="text-sm text-center mt-4">
+          Already have an account?{' '}
+          <Link to="/login" className="text-blue-500 hover:underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
     );
 };
 
