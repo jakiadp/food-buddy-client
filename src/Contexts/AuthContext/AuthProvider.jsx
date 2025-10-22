62125b72e300a39ceecd2754a42eff2fa853abba
Jakia Akter <jakiadp@gmail.com>
2025-10-23 00:06:50 +0600

    add to file

--- src/Contexts/AuthContext/AuthProvider.jsx
+++ src/Contexts/AuthContext/AuthProvider.jsx
@@ -3,15 +3,18 @@ import { AuthContext } from './Authcontext';
 import {  createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../Firebase/firebase.init';
 
+
 const AuthProvider = ({children}) => {
 
   const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) =>{
    setLoading(true);
-   return createUserWithEmailAndPassword(auth, email, password)
+   return createUserWithEmailAndPassword( auth, email, password)
 
-  }
+  };
+
+
 
 
   
