82c2533dcddde1e99b0aa2c3c126a757f6f5185a
Jakia Akter <jakiadp@gmail.com>
2025-12-10 23:22:29 +0600

    a

--- src/Component/add.jsx
+++ src/Component/add.jsx
@@ -3,21 +3,7 @@ import React from 'react';
 const add = () => {
     return (
         <div>
-            <h1>klklkkjk
-                klklkkjk
-                lkskkjjjka mm
-                llo
-                <loilo className="klkbk">kklc
-
-                    jjh
-                    jakia
-                    maksuda
-                    sagor
-                    async (params) => {
-                        
-                    }
-                </loilo>
-            </h1>
+            <h1>            </h1>
         </div>
     );
 };
