5f0ed01f95b3ba33c8448b504d5d3ee534cd9ba3
Jakia Akter <jakiadp@gmail.com>
2026-01-23 23:49:26 +0600

    add to

--- src/Pages/FoodApply/FoodApply.jsx
+++ src/Pages/FoodApply/FoodApply.jsx
@@ -14,6 +14,8 @@ console.log(jobId,user);
         <div>
             <h3 className='text-4xl'>Food Apply</h3>
             <h2>         food  buddy is comming soon </h2>
+            h2
+            <h2>ami achi onek dure</h2>
         </div>
     );
 };
